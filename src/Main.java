6b151fae27c4a0ad7ef6c1b3d01c053234aa97b5
ArtemaPI01 <klimaa22@yandex.ru>
2022-03-22 07:28:00 +0700

    3

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
 public class Main {
     public static void main(String[] args){
-        Owner alisher = new Owner();
         Fio fio = new Fio();
         fio.init("a","b", "c");
+        Owner alisher = new Owner();
         alisher.init(fio, 0,0);
         alisher.enter();
         alisher.print();
+        Shop ashan = new Shop();
+        ashan.enter();
+        ashan.print();
+        ashan.cloak();
+        
     }
 }
